e2ace3643e3fbae670284eb1238110b650a436a4
tarman1808 <tarman1808@gmail.com>
2018-10-06 23:56:18 +0700

    Initial commit

---
+++ src/com/schoolsystem/controller/LoginController.java
@@ -0,0 +1,51 @@
+package com.schoolsystem.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.schoolsystem.model.LoginModel;
+import com.schoolsystem.util.option;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    LoginModel loginModel = new LoginModel();
+    @FXML
+    private JFXTextField usernametf;
+
+    @FXML
+    private JFXPasswordField passwordtf;
+
+    @FXML
+    private JFXComboBox<option> cboxLogin;
+
+    @FXML
+    private JFXButton loginbtn;
+
+    @FXML
+    private Label dbStatus;
+
+    @FXML
+    private Label errorlogin;
+
+
+    public void  initialize (URL url, ResourceBundle rb){
+
+        if(this.loginModel.isDbConnected()){
+            this.dbStatus.setText("Connected");
+        }else {
+            this.dbStatus.setText("Not Connected");
+        }
+
+        this.cboxLogin.setItems(FXCollections.observableArrayList(option.values()));
+
+    }
+
+}
---
+++ src/com/schoolsystem/model/LoginModel.java
@@ -0,0 +1,62 @@
+package com.schoolsystem.model;
+
+import com.schoolsystem.util.SqliteHelper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginModel {
+
+    Connection connection ;
+
+    public LoginModel(){
+
+        try {
+            this.connection = SqliteHelper.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if(this.connection == null){
+            System.exit(1);
+        }
+    }
+
+    public boolean isDbConnected(){
+        return null != this.connection;
+
+    }
+
+    public  boolean isLogin(String user,String pass, String opt) throws  Exception{
+
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql ="SELECT * FROM login where username=? and password=? and role=?";
+
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1, user);
+            pr.setString(2, pass);
+            pr.setString(3,opt);
+
+
+            rs = pr.executeQuery();
+
+            boolean boll1;
+
+            if(rs.next()){
+                return  true;
+            }
+            return false;
+
+        }catch (SQLException e){
+            return false;
+        }finally {
+            pr.close();
+            rs.close();
+        }
+    }
+}
