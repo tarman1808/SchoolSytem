b00cc9aa9ebe2d21cbbdf44a954d6cc4f75db608
tarman1808 <tarman1808@gmail.com>
2018-10-07 22:38:09 +0700

    coding

--- src/LoginApp/LoginModel.java
+++ src/LoginApp/LoginModel.java
@@ -1,4 +1,58 @@
 package LoginApp;
 
+import Utils.SqliteHelper;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginModel {
+
+    Connection connection;
+
+    public LoginModel(){
+        try{
+            this.connection = SqliteHelper.getConnection();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        if(this.connection == null){
+            System.exit(1);
+        }
+    }
+
+    public boolean isLogin(String user, String pass, String opt) throws Exception{
+
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql = "SELECT * FROM login WHERE username=? and password=? and role=?";
+
+        try{
+
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1, user);
+            pr.setString(2, pass);
+            pr.setString(3, opt);
+
+            rs = pr.executeQuery();
+            boolean boll1;
+            if (rs.next()){
+                return true;
+            }
+            return false;
+
+        }catch (SQLException e){
+
+            e.printStackTrace();
+
+        }finally {
+            assert pr != null;
+            assert rs != null;
+
+        }
+
+        return false;
+    }
 }
